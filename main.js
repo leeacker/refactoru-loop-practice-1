3ef1bcb2130923a26c078e0f78c45a24876d0eef
Lee Acker <acker.lee4@gmail.com>
2014-09-15 16:12:40 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+for(var i=0 ; i<600; i+=100) {
+	console.log(i);
+};
+
+for(var i=1; i<65; i*=2) {
+	console.log(i);
+};
+
+for (var i=1; i<4; i++) {
+	console.log(i + "\n" + i + "\n" + i);
+};
+
+for (var i=0; i<11; i+=2){
+	console.log(i);
+};
+
+for (var i=3; i<16; i+=3){
+	console.log(i);
+};
+
+for (var i=9; i>-1; i--) {
+	console.log(i);
+};
+
+for(var j=0; j<3; j++)
+	for(var i=0; i<4; i++){
+	console.log(i);
+};
